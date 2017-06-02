17a83cefa0a14f03bd7dc6fa0707063de7171873
DAW122 <DAW122@C514.iesataulfo.local>
2017-06-02 19:45:49 +0200

    creada ventana 2

--- src/frames/menuAdmin.java
+++ src/frames/menuAdmin.java
@@ -17,11 +17,18 @@ public class menuAdmin extends JFrame{
     private JPanel pnl = new JPanel();
     //elementos
     private JLabel titulo = new JLabel("Menu");
+    private JLabel tit2 = new JLabel("Acciones");
+    
+    private JButton emp = new JButton("Nuevo empleado");
+    private JButton emp2 = new JButton("Nuevo ...");
+    private JButton emp3 = new JButton("Nuevo ...");
+    private JButton emp4 = new JButton("Nuevo ...");
+    
     
     public void inicio(){
         
         //frame
-        frm.setBounds(200, 200, 400, 170);
+        frm.setBounds(200, 200, 400, 400);
         frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         pnl.setLayout(null);
@@ -31,7 +38,24 @@ public class menuAdmin extends JFrame{
         frm.setVisible(true);
         
         // elementos
-        titulo.setBounds(100, 10, 100, 20);
+        titulo.setBounds(100, 10, 200, 20);
         pnl.add(titulo);
+        
+        tit2.setBounds(10,30,100,20);
+        pnl.add(tit2);
+        
+        emp.setBounds(10,50,150,30);
+        pnl.add(emp);
+        
+        emp2.setBounds(10,100,150,30);
+        pnl.add(emp2);
+        
+        emp3.setBounds(10,150,150,30);
+        pnl.add(emp3);
+        
+        emp4.setBounds(10,200,150,30);
+        pnl.add(emp4);
+
+        
     }
 }
